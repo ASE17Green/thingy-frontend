b04c8e27092bca7cd76a070e2d2a54f991b90af5
root <kevin.meister@students.unibe.ch>
2017-12-17 15:12:01 +0100

    improved dashboard

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 import { UserService, ThingyService, UserthingyService } from '../../shared/services/index';
 import { User, Userthingy, ThingyData } from '../../shared/models/index';
-import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,12 +13,8 @@ import {MatSnackBar, MatSnackBarConfig} from '@angular/material';
 export class DashboardComponent implements OnInit {
     username: string;
     user = new User();
-    dataNumber: number;
-    lastThingy: ThingyData;
-    userthingyStatus = 'Status unknown';
-    packageArrived = false;
-    showData = false;
-    userthingyLength = 0;
+    userthingys: Userthingy[] = [];
+    thingyEntries = [];
 
     // alert settings
     config = new MatSnackBarConfig();
@@ -44,66 +40,65 @@ export class DashboardComponent implements OnInit {
                 return null;
             }).then(
             () => {
-                if (this.user.userThingys) {
-                    this.thingyService.getLastEntry(this.user.userThingys[0]).then(
-                        (thingyData: ThingyData) => {
-                            this.lastThingy = thingyData;
-                        },
-                        error => {
-                            console.log('Something went wrong');
-                        });
-                }
-            }
-        ).then(
-            () => {
-                if (this.user.userThingys) {
-                    this.thingyService.getThingyById(this.user.userThingys[0]).then(
-                        (thingyData: ThingyData[]) => {
-                            this.dataNumber = thingyData.length;
-                            if(this.dataNumber === 0) {
-                                this.userthingyStatus = 'Ready for delivery';
-                            }
-                        },
-                        error => {
-                            console.log('Something went wrong');
-
-                        });
-                }
-            }
-        ).then(
-            () => {
-                for (let userthingyId of this.user.userThingys) {
-                    this.userthingyLength += 1;
-                    this.userthingyService.getUserthingyById(userthingyId).then(
-                        (userThingy: Userthingy) => {
-                            if (userThingy.packageArrivedMessageSent) {
-                                this.userthingyStatus = 'Package arrived';
-                                this.packageArrived = true;
-                            } else if (userThingy.thingyTemperatureMessageSent) {
-                                this.userthingyStatus = 'Reached critical temperature';
-                            } else if (this.dataNumber === 0) {
-                                this.userthingyStatus = 'Ready for delivery';
-                            } else {
-                                this.userthingyStatus = 'On its way';
+                    this.userthingyService.getUserthingys().then(
+                        (userThingys: Userthingy[]) => {
+                            this.userthingys = userThingys;
+                            for(let userthingy of this.userthingys){
+                                this.loadThingydata(userthingy.thingyID);
                             }
                         },
                         error => {
                             this.snackbar.open('Couldnt load userthingy');
                         });
-                }
             }
         );
         this.username = JSON.parse(localStorage.getItem('user')).name;
+    }
+
+    returnStatus(userThingy: Userthingy): string {
+        if (userThingy.packageArrivedMessageSent) {
+            return 'Package arrived';
+        } else if (userThingy.thingyTemperatureMessageSent) {
+            return 'Reached critical temperature';
+        } else if (userThingy.packageStartedMessageSent) {
+            return 'On its way';
+        } else {
+            return'Ready for delivery';
+        }
+    }
+
+    loadThingydata(id: string) {
+        this.thingyService.getThingyById(id).then(
+            (thingyData: ThingyData[]) => {
+                let thingyEntry = {thingyID: id, thingyEntries: thingyData.length};
+                this.thingyEntries.push(thingyEntry);
+            },
+            error => {
+                this.snackbar.open('Couldnt load userthingy');
+            });
+    }
+
+    getThingyEntries(id: string): string {
+        console.log(id);
+        let result = this.thingyEntries.filter(function( obj ) {
+            return obj.thingyID === id;
+        });
+        if (result[0]) {
+            return result[0].thingyEntries;
+        }else {
+            return '0';
+        }
+
     }
 
     onDeleteUserthingy(id: string) {
         this.userthingyService.deleteUserthingy(id).then(
             data => {
                 this.snackbar.open('Thingy ' + id + ' was successfully deleted.', 'close', this.config);
-                this.packageArrived = false;
                 this.userService.getUser().then(
                     userdata => {
                         this.user = userdata;
+                        this.ngOnInit();
                     },
                     error => {
                         this.snackbar.open('Something went wrong. Please contact an admin', 'close', this.config);
